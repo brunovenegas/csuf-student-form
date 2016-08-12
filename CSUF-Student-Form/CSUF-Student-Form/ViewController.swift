72ee65acaca5e3dd1e6dd329eb0a8e33e260340c
Bruno Venegas <bruno.venegas@gmail.com>
2016-08-11 20:43:35 -0500

    Hello, world!

---
+++ CSUF-Student-Form/CSUF-Student-Form/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CSUF-Student-Form
+//
+//  Created by Bruno Venegas on 8/11/16.
+//  Copyright © 2016 California State University, Fullerton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello, world!");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
