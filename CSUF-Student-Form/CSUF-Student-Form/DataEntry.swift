f41b92c0dcd7ae1656dd49ff48882ad080dccfbb
Dustin <lambright.dustin@gmail.com>
2016-08-12 05:31:57 -0400

    added stuff to .gitignore

---
+++ CSUF-Student-Form/CSUF-Student-Form/DataEntry.swift
@@ -0,0 +1,9 @@
+//
+//  DataEntry.swift
+//  CSUF-Student-Form
+//
+//  Created by Charlie Buckets on 8/11/16.
+//  Copyright © 2016 California State University, Fullerton. All rights reserved.
+//
+
+import Foundation
