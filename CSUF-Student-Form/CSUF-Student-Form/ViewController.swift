69e7142217eed2bc82580bf2dfdecfd8df58e896
Bruno Venegas <bruno.venegas@gmail.com>
2016-08-13 14:00:53 -0500

    Variablers implemented in code

--- CSUF-Student-Form/CSUF-Student-Form/ViewController.swift
+++ CSUF-Student-Form/CSUF-Student-Form/ViewController.swift
@@ -9,16 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Text Fields
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var majorTextField: UITextField!
+    
+    // Labels
+    @IBOutlet weak var programsOfStudyLabel: UILabel!
+    @IBOutlet weak var startTermLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        initializeVariables();
+        
         print("Hello, world!");
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func initializeVariables() {
+      //  programsOfStudyLabel.lineBreakMode = .ByWordWrapping;
+     //   programsOfStudyLabel.numberOfLines = 0;
+        
     }
 
 
