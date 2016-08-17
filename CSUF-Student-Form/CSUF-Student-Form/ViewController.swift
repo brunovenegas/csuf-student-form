c63dc8652926770664b194379264770e8e99d2bf
Bruno Venegas <bruno.venegas@gmail.com>
2016-08-16 20:49:44 -0500

    App can save and retrieve CSV file data. Need to find a way to export actual file.

--- CSUF-Student-Form/CSUF-Student-Form/ViewController.swift
+++ CSUF-Student-Form/CSUF-Student-Form/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     // Text Fields
@@ -33,6 +34,12 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     var graduateBool: Bool = false
     var summerBool: Bool = false
     var otherBool: Bool = false
+    var englishString: String = "No"
+    var undergradString: String = "No"
+    var graduateString: String = "No"
+    var summerString: String = "No"
+    var otherString: String = "No"
+    var summerProgramArray: NSMutableArray = []
     
     // Picker View
     @IBOutlet var startTermPickerView: UIPickerView!
@@ -44,17 +51,16 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     let checkedImage = UIImage(named: "checkedbox")! as UIImage
     let uncheckedImage = UIImage(named: "uncheckedbox")! as UIImage
     
-    // Data Entry
-    var newDataEntry: [DataEntry] = []
+    // CSV Stuff
+    let csvHeaderString: String = "FirstName,LastName,Email,IntensiveEnglish,Undergrad,Graduate,Summer,Other,Major,StartTerm\n"
+    var userDataString: String = ""
+    var didFilePreviouslyExist: Bool = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
         initializeVariables();
-       
-    //    Model.setDummyData()
-    //    Model.loadDummyData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -68,6 +74,13 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         
         // Display e-mail friendly keyboard
         emailTextField.keyboardType = UIKeyboardType.EmailAddress
+        
+        // Remove autocorrection on text fields
+        firstNameTextField.autocorrectionType = .No
+        lastNameTextField.autocorrectionType = .No
+        emailTextField.autocorrectionType = .No
+        majorTextField.autocorrectionType = .No
+        
         
         //Looks for single or multiple taps in view
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
@@ -92,66 +105,69 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         if(sender == intensiveEnglishCheckbox) {
             if(englishBool == true) {
                 englishBool = false
+                englishString = "No"
                 intensiveEnglishCheckbox.setImage(uncheckedImage, forState: UIControlState.Normal)
             }
             else if(englishBool == false) {
                 englishBool = true
+                englishString = "Yes"
                 intensiveEnglishCheckbox.setImage(checkedImage, forState: UIControlState.Normal)
             }
         }
         else if(sender == undergraduateCheckbox) {
             if(undergradBool == true) {
                 undergradBool = false
+                undergradString = "No"
                 undergraduateCheckbox.setImage(uncheckedImage, forState: UIControlState.Normal)
             }
             else if(undergradBool == false) {
                 undergradBool = true
+                undergradString = "Yes"
                 undergraduateCheckbox.setImage(checkedImage, forState: UIControlState.Normal)
             }
         }
         else if(sender == graduateCheckbox) {
             if(graduateBool == true) {
                 graduateBool = false
+                graduateString = "No"
                 graduateCheckbox.setImage(uncheckedImage, forState: UIControlState.Normal)
             }
             else if(graduateBool == false) {
                 graduateBool = true
+                graduateString = "Yes"
                 graduateCheckbox.setImage(checkedImage, forState: UIControlState.Normal)
             }
         }
         else if(sender == summerProgramCheckbox) {
             if(summerBool == true) {
                 summerBool = false
+                summerString = "No"
                 summerProgramCheckbox.setImage(uncheckedImage, forState: UIControlState.Normal)
             }
             else if(summerBool == false) {
                 summerBool = true
+                summerString = "Yes"
                 summerProgramCheckbox.setImage(checkedImage, forState: UIControlState.Normal)
             }
         }
         else if(sender == otherCheckbox) {
             if(otherBool == true) {
                 otherBool = false
+                otherString = "No"
                 otherCheckbox.setImage(uncheckedImage, forState: UIControlState.Normal)
             }
             else if(otherBool == false) {
                 otherBool = true
+                otherString = "Yes"
                 otherCheckbox.setImage(checkedImage, forState: UIControlState.Normal)
             }
         }
         else if(sender == saveButton) {
-            // Save all data and clear everything
-        //    newDataEntry.append(<#T##newElement: Element##Element#>)
-            print(firstNameTextField.text)
-            print(lastNameTextField.text)
-            print(emailTextField.text)
-            print(summerBool.boolValue)
-            print(undergradBool.boolValue)
-            print(graduateBool.boolValue)
-            print(summerBool.boolValue)
-            print(otherBool.boolValue)
-            print(majorTextField.text)
-            print(selectedTermString)
+            userDataString = String(format: "\(firstNameTextField.text!),\(lastNameTextField.text!),\(emailTextField.text!),\(englishString),\(undergradString),\(graduateString),\(summerString),\(otherString),\(majorTextField.text!),\(selectedTermString)\n")
+            
+            saveInfo(userDataString)
+            resetUI()
+            retrieveInfo()
         }
         else {
             print("WTF are we doing in here?")
@@ -188,7 +204,53 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         selectedTermString = pickerViewData[row]
     }
-
+    
+    // Save CSV info
+    func saveInfo(userString: String) {
+        let DocumentsDirectory: String = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).first!
+        let ArchiveURL: String = DocumentsDirectory.stringByAppendingString("/studentinfo.csv")
+        if(!NSFileManager.defaultManager().fileExistsAtPath(ArchiveURL)) {
+            NSFileManager.defaultManager().createFileAtPath(ArchiveURL, contents: nil, attributes: nil)
+            didFilePreviouslyExist = false;
+        }
+        let fileHandle = NSFileHandle(forUpdatingAtPath: ArchiveURL)
+        fileHandle!.seekToEndOfFile()
+        if(!didFilePreviouslyExist) {
+            fileHandle!.writeData(csvHeaderString.dataUsingEncoding(NSUTF8StringEncoding)!)
+        }
+        fileHandle!.writeData(userString.dataUsingEncoding(NSUTF8StringEncoding)!)
+        fileHandle!.closeFile()
+    }
+    
+    // Retreive CSV info
+    func retrieveInfo() {
+        let DocumentsDirectory: String = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true).first!
+        let ArchiveURL: String = DocumentsDirectory.stringByAppendingString("/studentinfo.csv")
+        if(NSFileManager.defaultManager().fileExistsAtPath(ArchiveURL)) {
+            let fileHandle  = NSFileHandle(forReadingAtPath: ArchiveURL)
+            let results: String = String(data: fileHandle!.availableData, encoding: NSUTF8StringEncoding)!
+            fileHandle?.closeFile()
+            print(results)
+        }
+    }
+    
+    // Clears text boxes, resets booleans, clears checkboxes
+    func resetUI() {
+        firstNameTextField.text = ""
+        lastNameTextField.text = ""
+        emailTextField.text = ""
+        majorTextField.text = ""
+        englishBool = false;
+        undergradBool = false;
+        graduateBool = false;
+        summerBool = false;
+        otherBool = false;
+        intensiveEnglishCheckbox.setImage(uncheckedImage, forState: UIControlState.Normal)
+        undergraduateCheckbox.setImage(uncheckedImage, forState: UIControlState.Normal)
+        graduateCheckbox.setImage(uncheckedImage, forState: UIControlState.Normal)
+        summerProgramCheckbox.setImage(uncheckedImage, forState: UIControlState.Normal)
+        otherCheckbox.setImage(uncheckedImage, forState: UIControlState.Normal)
+    }
 
 
 }
